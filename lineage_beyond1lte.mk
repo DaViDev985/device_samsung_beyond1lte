baad2e53eec9d1b304047f9b22d234123fbb47f8
DaViDev985 <davidev985@gmail.com>
2025-06-28 20:08:26 +0200

    beyond1lte: Spoof manufacturer as Google This makes Quick Share and certain Samsung apps (like Galaxy Wearable) work again.

--- lineage_beyond1lte.mk
+++ lineage_beyond1lte.mk
@@ -39,8 +39,8 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_beyond1lte
 PRODUCT_DEVICE := beyond1lte
-PRODUCT_BRAND := samsung
+PRODUCT_BRAND := google
 PRODUCT_MODEL := SM-G973F
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_MANUFACTURER := google
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
